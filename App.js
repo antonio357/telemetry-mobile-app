c2f5c42932fbf205dea454ac7547c75ecc2da948
antonio357 <carlosalbuquerque359@gmail.com>
2022-09-07 22:32:18 -0300

    websocket conects but it closes connection after while

--- App.js
+++ App.js
@@ -1,11 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// websocket
+// turtorials at
+// https://www.youtube.com/watch?v=cfggyE1Ptbc&ab_channel=ReactNativeTutorial
+// https://www.youtube.com/watch?v=PyNdOCt6i6Q&t=721s&ab_channel=CollabCoding
+// window.navigator.userAgent = 'react-native'; // it may need to prevent bugs
+import io from 'socket.io-client';
+
+// websocket connecting
+const ws = io("ws://192.168.1.199:81", { 
+  transports: ["websocket"], // ensure it will use websocke instead of polling which is for http and its slower
+  multiplex: true, // avoid createing new connection if not necessary
+});
+
+// websocket variables to show status
+const showEvents = true
+const wsEvents = [
+  "error",
+  "reconnect",
+  "reconnect_attempt",
+  "reconnect_error",
+  "reconnect_failed",
+  "ping",
+  "hey",
+  "connect",
+  "connection",
+  "disconnect",
+  "upgrade",
+  "packet",
+  "packetCreate",
+  "drain",
+  "close",
+  "data",
+  "connect_error",
+]
+
 export default function App() {
+  // websocket variables to show status
+  const [counter, setCounter] = useState(1);
+  const [conncetionStatus, setConnectionStatus] = useState({});
+
+  // using websocket variables to show status
+  if (showEvents) {
+    wsEvents.map(event => {
+      ws.on(event, eventObj => {
+        if (counter <= 0) return false
+        setCounter(counter - 1)
+        let status = conncetionStatus
+        if (status[event]) status[event].push(eventObj)
+        else status[event] = [eventObj]
+        setConnectionStatus({...status})
+      })
+    })
+  } else {
+    wsEvents.map(event => {
+      ws.on(event, () => {
+        if (counter <= 0) return false 
+        setCounter(counter - 1)
+        let status = conncetionStatus
+        if (status[event]) status[event] = status[event] + 1
+        else status[event] = 1
+        setConnectionStatus({...status})
+      })
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Connection status: {`${JSON.stringify(conncetionStatus)}`}</Text>
+      <Text>Connection props: {Object.keys(ws).map(item => ` ${item} `)}</Text>
       <StatusBar style="auto" />
     </View>
   );
