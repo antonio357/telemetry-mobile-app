6ad91e887bdfd97bf3aabe172e07d2964aa6d7c8
antonio357 <carlosalbuquerque359@gmail.com>
2022-09-18 18:24:32 -0300

    routes definition

---
+++ src/screens/sensores/Sensores.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+import { styles } from '../../../AppStyles';
+
+
+export default function Sensores() {
+  return (
+    <View style={styles.view}>
+      <Text>Sensores Screen</Text>
+    </View> 
+  );
+}
