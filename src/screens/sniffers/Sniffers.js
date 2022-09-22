3cd55209fc418eab09a0347818b6273ba14ebeae
antonio357 <carlosalbuquerque359@gmail.com>
2022-09-21 23:31:48 -0300

    template for sniffers

--- src/screens/sniffers/Sniffers.js
+++ src/screens/sniffers/Sniffers.js
@@ -1,15 +1,52 @@
-import { View, Text } from "react-native";
+import React from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from "react-native";
 import { ScreenBase } from "../common/ScreenBase";
 import { Sniffer } from "../../components/sniffer/Sniffer";
 
 import { styles } from '../../../App.styles';
 
 
+const stylesSniffer = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 20,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
+
+let DATA = [];
+for (let index = 0; index < 30; index++) {
+  DATA.push({ id: `${index}`, title: `item ${index}` })
+}
+
+
+const Item = ({ title }) => (
+  <View style={stylesSniffer.item}>
+    <Text style={stylesSniffer.title}>{title}</Text>
+  </View>
+);
+
 export default function Sniffers({ navigation }) {
+  const renderItem = ({ item }) => (
+    <Item title={item.title} />
+  );
+
   return (
-    <View style={styles.view}>
-      <ScreenBase openRoutesMenu={() => navigation.openDrawer()}/>
-      <Sniffer status={"static status"}/>
-    </View>
-  )
+    <SafeAreaView style={stylesSniffer.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+      <ScreenBase openRoutesMenu={() => navigation.openDrawer()} />
+    </SafeAreaView>
+  );
 }
