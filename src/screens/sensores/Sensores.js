9c7395439101454d62ce0f64897cf34728ec5686
antonio357 <carlosalbuquerque359@gmail.com>
2022-10-06 10:29:40 -0300

    organizando botões

--- src/screens/sensores/Sensores.js
+++ src/screens/sensores/Sensores.js
@@ -1,9 +1,7 @@
 import { View, Text, Button, ScrollView, Dimensions } from "react-native";
 import { ScreenBase } from "../common/ScreenBase";
 import { observer, inject } from 'mobx-react';
-import { styles } from '../../../App.styles';
-import { Canvas, Skia, Path, SkiaView } from "@shopify/react-native-skia";
-import { useState } from "react";
+import { Canvas, Path } from "@shopify/react-native-skia";
 
 
 function Sensores({ navigation, RegisteredSniffersStore }) {
@@ -15,34 +13,26 @@ function Sensores({ navigation, RegisteredSniffersStore }) {
   const viewWidth = screeWidth - (2 * viewMarging);
   const drawWidth = viewWidth - (strokeWidth * 2);
 
-  // let chartsArray = [];
-  // const allPortChart = getAllPortChart();
-  // for (let i = 0; i < allPortChart.length; i++) {
-  //   chartsArray.push(
-  //     <Canvas key={`${allPortChart[i].url}${allPortChart[i].port}`} style={{ width: viewWidth, height: viewWidth }} mode='continuous' debug={true} >
-  //       <Path path={allPortChart[i].chart.getPath()} style="stroke" color="tomato" strokeWidth={3} />
-  //     </Canvas>
-  //   );
-  // }
-
   return (
-    <View style={{ margin: viewMarging, width: viewWidth }}>
-      <Button title={lastCmdToAllWsClients == "stop logs" ? "get logs" : "stop logs"} onPress={() => {
-        if (lastCmdToAllWsClients == "stop logs") {
-          startLogs();
-        } else {
-          stopLogs();
-        }
-      }} />
-      <ScrollView>
-        {getAllPortChart().map(portChart => {
-          return (
-            <Canvas key={`${portChart.url}${portChart.port}`} style={{ width: viewWidth, height: viewWidth }} mode='continuous' debug={true} >
-              <Path path={portChart.chart.getPath()} style="stroke" color="tomato" strokeWidth={3} />
-            </Canvas>
-          );
-        })}
-      </ScrollView>
+    <View style={{ flex: 1 }}>
+      <View style={{ margin: viewMarging }}>
+        <Button title={lastCmdToAllWsClients == "stop logs" ? "get logs" : "stop logs"} onPress={() => {
+          if (lastCmdToAllWsClients == "stop logs") {
+            startLogs();
+          } else {
+            stopLogs();
+          }
+        }} />
+        <ScrollView style={{ margin: viewMarging, width: viewWidth }}>
+          {getAllPortChart().map(portChart => {
+            return (
+              <Canvas key={`${portChart.url}${portChart.port}`} style={{ width: viewWidth, height: viewWidth }} mode='continuous' debug={true} >
+                <Path path={portChart.chart.getPath()} style="stroke" color="tomato" strokeWidth={3} />
+              </Canvas>
+            );
+          })}
+        </ScrollView>
+      </View>
       <ScreenBase openRoutesMenu={() => navigation.openDrawer()} />
     </View>
   );
