be2d13f21ee37b4ee54c84e2fec4947d1dc0fe2b
antonio357 <carlosalbuquerque359@gmail.com>
2022-10-21 02:45:35 -0300

    commenting old tests

--- src/screens/recording/Recording.js
+++ src/screens/recording/Recording.js
@@ -1,44 +1,40 @@
 import { View } from "react-native";
 import { ScreenBase } from "../common/ScreenBase";
 import { styles } from '../../../App.styles';
-import { ScrollView } from "react-native-gesture-handler";
-import { StyleSheet } from "react-native";
 import { observer, inject } from 'mobx-react';
-import { Sensores } from "../sensores/Sensores";
-import { Skia } from "@shopify/react-native-skia";
 import { ChartCardsList } from '../../charts/ChartCardsList';
 import { ChartDrawPath } from '../../charts/ChartDrawPath';
 
-const path1 =  new ChartDrawPath('ultrassonic');
-const path2 =  new ChartDrawPath('touch');
-const array = [
-  {
-    sensorName: "sensor de distância",
-    sensorType: "ultrassonic",
-    timeFrame: 10, // faixa de tempo do eixo x em segundos
-    logsRate: 1000, // quantidade dos logs por segundo
-    drawPath: path1.getPath()
-  },
-  {
-    sensorName: "sensor de batida",
-    sensorType: "touch",
-    timeFrame: 10, // faixa de tempo do eixo x em segundos
-    logsRate: 1000, // quantidade dos logs por segundo
-    drawPath: path2.getPath()
-  },
-];
-let timeCounter = 0;
-const thread = setInterval(() => {
-  let array1 = [];
-  let array2 = [];
-  for (let i = 0; i < 20; i++) {
-    array1.push({value: Math.random() * 256, time: timeCounter});
-    array2.push({value: Math.random() * 2, time: timeCounter});
-    timeCounter += 1;
-  }
-  path1.loadDataVector(array1);
-  path2.loadDataVector(array2);
-}, 20);
+// const path1 =  new ChartDrawPath('ultrassonic');
+// const path2 =  new ChartDrawPath('touch');
+// const array = [
+//   {
+//     sensorName: "sensor de distância",
+//     sensorType: "ultrassonic",
+//     timeFrame: 10, // faixa de tempo do eixo x em segundos
+//     logsRate: 1000, // quantidade dos logs por segundo
+//     drawPath: path1.getPath()
+//   },
+//   {
+//     sensorName: "sensor de batida",
+//     sensorType: "touch",
+//     timeFrame: 10, // faixa de tempo do eixo x em segundos
+//     logsRate: 1000, // quantidade dos logs por segundo
+//     drawPath: path2.getPath()
+//   },
+// ];
+// let timeCounter = 0;
+// const thread = setInterval(() => {
+//   let array1 = [];
+//   let array2 = [];
+//   for (let i = 0; i < 20; i++) {
+//     array1.push({value: Math.random() * 256, time: timeCounter});
+//     array2.push({value: Math.random() * 2, time: timeCounter});
+//     timeCounter += 1;
+//   }
+//   path1.loadDataVector(array1);
+//   path2.loadDataVector(array2);
+// }, 20);
 
 function Recording({ navigation, RegisteredSniffersStore }) {
   // const { getAllPortChart } = RegisteredSniffersStore;
@@ -46,7 +42,7 @@ function Recording({ navigation, RegisteredSniffersStore }) {
 
   return (
     <View style={styles.view}>
-      <ChartCardsList sensorConfigsArray={array} />
+      {/* <ChartCardsList sensorConfigsArray={array} /> */}
       <ScreenBase openRoutesMenu={() => navigation.openDrawer()} />
     </View>
   )
