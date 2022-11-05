b52700581fd22a8ce272cc1d1dc9339877d6ed4d
antonio357 <carlosalbuquerque359@gmail.com>
2022-11-05 17:21:28 -0300

    cleanning code

--- App.js
+++ App.js
@@ -7,20 +7,7 @@ import DbOperations from './src/database/DbOperations';
 
 
 setTimeout(async () => {
-  await DbOperations.initTables(true);
-  // console.log(`logs table had ${await Logs.countRecords()} records on inicialization`);
-  // await Logs.deleteAllRecords();
-  // console.log(`logs table has ${await Logs.countRecords()} records after inicialization`);
-  // console.log(`logs = ${(await Logs.getAllRecords()).length}`);
-  // const logs = [];
-  // const quantLogs = 10000;
-  // for (let i = 0; i < quantLogs; i++) {
-  //   logs.push({ value: `${parseInt(Math.random() * 2551) / 10}`, time: 500 })
-  // }
-  // const initialTime = new Date().getTime();
-  // await Logs.appendLogs(logs);
-
-  // console.log(`after ${(new Date().getTime()) - initialTime} ms inserting ${quantLogs} logs, logs table has ${await Logs.countRecords()} records`);
+  await DbOperations.initTables();
 });
 
 function App() {
