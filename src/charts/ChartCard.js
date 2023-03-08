382057500f146898a7076b26722d1fca352962e3
Antonio Albuqeurque <carlosalbuquerque359@gmail.com>
2023-03-07 23:49:58 -0300

    y axis for ultrasonic sensor

--- src/charts/ChartCard.js
+++ src/charts/ChartCard.js
@@ -6,13 +6,24 @@ import { CanvasDimensions } from './CanvasDimensions';
 const dimensions = new CanvasDimensions();
 
 const styles = StyleSheet.create({
-  view: { flex: 1 },
-  text: {},
   canvas: { height: dimensions.canvasDimensions.height, width: dimensions.canvasDimensions.width },
   yAxisDrawPath: {},
   drawPath: {},
   xAxisDrawPath: {},
+  yAxisCanvas: { flexDirection: 'row' },
+  yAxisText: { flex: 1, flexDirection: 'column', height: 68, fontSize: 12, textAlign: "right" }
 });
+
+function YAxisLabel() {
+  return (
+    <View style={styles.yAxis}>
+      <Text key={1} style={styles.yAxisText}>255</Text>
+      <Text key={2} style={styles.yAxisText}>128</Text>
+      <Text key={3} style={styles.yAxisText}>64</Text>
+      <Text key={4} style={styles.yAxisText}>0</Text>
+    </View>
+  )
+}
 
 export function ChartCard({ sensorConfig }) {
   // const sensorConfig = {
@@ -24,13 +35,16 @@ export function ChartCard({ sensorConfig }) {
   // }
 
   return (
-    <View style={styles.view}>
-      <Text>{sensorConfig.sensorName} : {sensorConfig.sensorType}</Text>
-      <Canvas style={styles.canvas} mode='continuous' debug={true}>
-        {/* path pro eixo y */}
-        <Path path={sensorConfig.drawPath} style="stroke" color="tomato" strokeWidth={1} />
-        {/* path pro eixo x */}
-      </Canvas>
+    <View>
+      <Text style={styles.chartName} >{sensorConfig.sensorName} : {sensorConfig.sensorType}</Text>
+      <View style={styles.yAxisCanvas}>
+        <YAxisLabel />
+        <Canvas style={styles.canvas} mode='continuous' debug={true}>
+          {/* path pro eixo y */}
+          <Path path={sensorConfig.drawPath} style="stroke" color="tomato" strokeWidth={1} />
+          {/* path pro eixo x */}
+        </Canvas>
+      </View>
     </View>
   );
 }
